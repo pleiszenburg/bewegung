8088786dd5876ee80f0feb7efc5572d55170043e
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-08-11 22:01:01 +0200

    draft video class

--- src/bewegung/core/video.py
+++ src/bewegung/core/video.py
@@ -23,3 +23,84 @@ specific language governing rights and limitations under the License.
 </LICENSE_BLOCK>
 
 """
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from typing import Callable
+
+from typeguard import typechecked
+
+from .time import Time
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# "GLOBALS" (FOR WORKERS)
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# _context = {}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CLASS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+@typechecked
+class Video:
+    """
+    Mutable. Decorators ...
+    """
+
+    def __init__(self, time: Time):
+
+        self._time = time
+
+        self._sequences = [] # list of sequences
+        self._layers = [] # list of layers
+
+    def sequence(self, start: Time, stop: Time) -> Callable:
+
+        @typechecked
+        def decorator(cls: type):
+
+            @typechecked
+            class wrapper(cls): # sequence class, setting time properties
+                def __init__(other, *args, **kwargs):
+                    other._start, other._stop = start, stop
+                    super().__init__(*args, **kwargs)
+
+            self._sequences.append(wrapper) # track sequence classes
+            return None # wrapper # HACK remove original class?
+
+        return decorator
+
+    def layer(self, index: int) -> Callable:
+
+        @typechecked
+        def decorator(func: Callable):
+
+            @typechecked
+            def wrapper(other, time: Time):
+                func.__globals__['time'] = time # inject time into namespace
+                try:
+                    ret = func(other)
+                finally:
+                    func.__globals__.pop('time') # cleanup namespace
+                return ret
+
+            wrapper.layer = index # tag wrapper function
+            return wrapper
+
+        return decorator
+
+    def render(self):
+
+        self._sequences[:] = [sequence() for sequence in self._sequences] # init sequences
+        self._layers.extend([
+            (getattr(sequence, attr).layer, getattr(sequence, attr))
+            for sequence in self._sequences for attr in dir(sequence)
+            if hasattr(getattr(sequence, attr), 'layer')
+        ]) # find layer methods based on tags
+
+        for frame in range(self._time):
+            for idx, layer in self._layers:
+                print( frame, idx, layer(frame) ) # call layer render functions
