9b63e5815ec13c7c285c101c06f4bb0c7cac604b
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-08-11 21:42:18 +0200

    header

--- src/bewegung/core/video_old.py
+++ src/bewegung/core/video_old.py
@@ -6,7 +6,7 @@ BEWEGUNG
 a versatile video renderer
 https://github.com/pleiszenburg/bewegung
 
-    src/bewegung/core/video.py: Parallel video frame renderer
+    src/bewegung/core/video_old.py: Parallel video frame renderer
 
     Copyright (C) 2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
