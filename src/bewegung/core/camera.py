106ae153d69ba9bd523fc664804d8691c2f38a3c
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-08-11 13:37:52 +0200

    camera runs on new array types; clean jit implementation

--- src/bewegung/core/camera.py
+++ src/bewegung/core/camera.py
@@ -30,7 +30,6 @@ specific language governing rights and limitations under the License.
 
 import math
 import sys
-import typing
 
 from typeguard import typechecked
 
@@ -38,17 +37,78 @@ import numpy as np
 import numba as nb
 
 from .abc import CameraABC
-from .vector import Vector2D, Vector3D
+from .vector import (
+    Vector2D,
+    Vector2Ddist,
+    Vector3D,
+    VectorArray2Ddist,
+    VectorArray3D,
+    )
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# TYPES
+# ROUTINES (JIT)
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-@typechecked
-class VectorDist(typing.NamedTuple):
+@nb.jit(
+    [
+        (
+            nb.float32[:, :], nb.float32[:, :],
+            nb.float32[:, :], nb.float32[:], nb.float32[:], nb.float32[:],
+            nb.float32, nb.boolean,
+        ), (
+            nb.float64[:, :], nb.float64[:, :],
+            nb.float64[:, :], nb.float64[:], nb.float64[:], nb.float64[:],
+            nb.float64, nb.boolean,
+        )
+    ],
+    nopython = True,
+)
+def _get_points_jit(
+    points_3d, points_2d,
+    ma, position, empty, planeOffset,
+    planeFactor, planeYFlip,
+    ):
 
-    vector: typing.Union[Vector2D, Vector3D]
-    dist: float
+    for index in range(0, points_3d.shape[0]):
+
+        ma[:, 2] = position - points_3d[index, :]
+
+        determ = (
+              ma[0][0] * ma[1][1] * ma[2][2]
+            + ma[0][1] * ma[1][2] * ma[2][0]
+            + ma[0][2] * ma[1][0] * ma[2][1]
+            - ma[0][2] * ma[1][1] * ma[2][0]
+            - ma[0][0] * ma[1][2] * ma[2][1]
+            - ma[0][1] * ma[1][0] * ma[2][2]
+            )
+
+        if determ == 0:
+            points_2d[index, :] = empty
+            continue
+
+        points_2d[index, 0] = (
+              ma[0][3] * ma[1][1] * ma[2][2]
+            + ma[0][1] * ma[1][2] * ma[2][3]
+            + ma[0][2] * ma[1][3] * ma[2][1]
+            - ma[0][2] * ma[1][1] * ma[2][3]
+            - ma[0][3] * ma[1][2] * ma[2][1]
+            - ma[0][1] * ma[1][3] * ma[2][2]
+            )
+        points_2d[index, 1] = (
+              ma[0][0] * ma[1][3] * ma[2][2]
+            + ma[0][3] * ma[1][2] * ma[2][0]
+            + ma[0][2] * ma[1][0] * ma[2][3]
+            - ma[0][2] * ma[1][3] * ma[2][0]
+            - ma[0][0] * ma[1][2] * ma[2][3]
+            - ma[0][3] * ma[1][0] * ma[2][2]
+            )
+
+        points_2d[index, :2] *= planeFactor / determ
+        if planeYFlip:
+            points_2d[index, 1] = -points_2d[index, 1]
+        points_2d[index, :2] += planeOffset
+
+        points_2d[index, 2] = np.sqrt(np.sum(np.power(points_3d[index, :] - position, 2))) # distance
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # CLASS
@@ -74,6 +134,7 @@ class Camera(CameraABC):
         # place Y flip
         self._planeYFlip = False
 
+        # compute rendering plane
         self._update_plane()
 
     def _update_plane(self):
@@ -143,7 +204,7 @@ class Camera(CameraABC):
     def planeYFlip(self, value: bool):
         self._planeYFlip = value
 
-    def get_point(self, point3D: Vector3D) -> VectorDist:
+    def get_point(self, point3D: Vector3D) -> Vector2Ddist:
 
         ma = [
             [self._planeX.x, self._planeY.x, -(point3D.x - self._position.x), -self._direction.x],
@@ -187,85 +248,41 @@ class Camera(CameraABC):
 
         point2D.x *= self._planeFactor
         point2D.y *= self._planeFactor
+        point2D += self._planeOffset
 
-        return VectorDist(
-            vector = point2D + self._planeOffset,
+        return Vector2Ddist(
+            x = point2D.x,
+            y = point2D.y,
             dist = (point3D - self._position).mag,
             )
 
-    def compiled_get_points(self) -> typing.Callable:
+    def get_points(self, points_3d: VectorArray3D) -> VectorArray2Ddist:
 
-        def get_points(points_3d: np.ndarray, points_2d: np.ndarray):
+        position = self._position.as_ndarray() # type
+        planeOffset = self._planeOffset.as_ndarray() # type
+        points_3d = points_3d.as_ndarray() # type
+        planeFactor = np.float32(self._planeFactor) # type
 
-            factor = np.float32(self._planeFactor)
-            flip = self._planeYFlip
+        ma = np.array([
+            [self._planeX.x, self._planeY.x, 0.0, -self._direction.x],
+            [self._planeX.y, self._planeY.y, 0.0, -self._direction.y],
+            [self._planeX.z, self._planeY.z, 0.0, -self._direction.z],
+            ], dtype = points_3d.dtype) # type / matrix
+        empty = np.array([
+            np.nan,
+            np.nan,
+            np.nan,
+            ], dtype = points_3d.dtype) # NaN placeholder
+        points_2d = np.zeros(points_3d.shape, dtype = points_3d.dtype) # type / target
 
-            @nb.jit((
-                nb.float32[:, :], nb.float32[:, :],
-                nb.float32[:, :], nb.float32[:], nb.float32[:], nb.float32[:],
-                ), nopython = True)
-            def get_points_jit(
-                points_3d, points_2d,
-                ma, position, empty, offset,
-                ):
+        _get_points_jit(
+            points_3d, points_2d,
+            ma, position, empty, planeOffset,
+            planeFactor, self._planeYFlip,
+            )
 
-                for index in range(0, points_3d.shape[1]):
-
-                    ma[:, 2] = position - points_3d[:, index]
-
-                    determ = (
-                          ma[0][0] * ma[1][1] * ma[2][2]
-                        + ma[0][1] * ma[1][2] * ma[2][0]
-                        + ma[0][2] * ma[1][0] * ma[2][1]
-                        - ma[0][2] * ma[1][1] * ma[2][0]
-                        - ma[0][0] * ma[1][2] * ma[2][1]
-                        - ma[0][1] * ma[1][0] * ma[2][2]
-                        )
-
-                    if determ == 0:
-                        points_2d[:, index] = empty
-                        continue
-
-                    points_2d[0, index] = (
-                          ma[0][3] * ma[1][1] * ma[2][2]
-                        + ma[0][1] * ma[1][2] * ma[2][3]
-                        + ma[0][2] * ma[1][3] * ma[2][1]
-                        - ma[0][2] * ma[1][1] * ma[2][3]
-                        - ma[0][3] * ma[1][2] * ma[2][1]
-                        - ma[0][1] * ma[1][3] * ma[2][2]
-                        )
-                    points_2d[1, index] = (
-                          ma[0][0] * ma[1][3] * ma[2][2]
-                        + ma[0][3] * ma[1][2] * ma[2][0]
-                        + ma[0][2] * ma[1][0] * ma[2][3]
-                        - ma[0][2] * ma[1][3] * ma[2][0]
-                        - ma[0][0] * ma[1][2] * ma[2][3]
-                        - ma[0][3] * ma[1][0] * ma[2][2]
-                        )
-
-                    points_2d[:2, index] *= factor / determ
-
-                    if flip:
-                        points_2d[1, index] = -points_2d[1, index]
-
-                    points_2d[:2, index] += offset
-
-                    points_2d[2, index] = np.sqrt(np.sum(np.power(points_3d[:, index] - position, 2))) # distance
-
-            ma = np.array([
-                [self._planeX.x, self._planeY.x, 0.0, -self._direction.x],
-                [self._planeX.y, self._planeY.y, 0.0, -self._direction.y],
-                [self._planeX.z, self._planeY.z, 0.0, -self._direction.z],
-                ], dtype = 'f4')
-            position = self._position.as_ndarray()
-            empty = np.array([
-                np.nan,
-                np.nan,
-                ], dtype = 'f4')
-            offset = self._planeOffset.as_ndarray()
-            get_points_jit(
-                points_3d, points_2d,
-                ma, position, empty, offset,
-                )
-
-        return get_points
+        return VectorArray2Ddist(
+            x = points_2d[:, 0],
+            y = points_2d[:, 1],
+            dist = points_2d[:, 2],
+            )
