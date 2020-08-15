cacf5b21724d8de42a962487075710396f581a33
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-08-15 20:40:22 +0200

    demo

---
+++ demo.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+BEWEGUNG
+a versatile video renderer
+https://github.com/pleiszenburg/bewegung
+
+    demo.py: Package demo
+
+    Copyright (C) 2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/bewegung/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from bewegung import Time, Video
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ROUTINES
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+def main():
+
+    v = Video(
+        time = Time.from_time(fps = 60, time = 10.0),
+        width = 1920,
+        height = 1080,
+        ctx = {
+            'background_color': (0.1, 0.1, 0.1, 1.0),
+        }
+    )
+
+    @v.sequence()
+    class Background:
+        @v.layer(
+            zindex = v.zindex.on_bottom(),
+            canvas = v.db_canvas(background_color = v.ctx['background_color']),
+        )
+        def empty(self, canvas):
+            return canvas
+
+    v.render(parallel = False)
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ENTRY
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+if __name__ == '__main__':
+
+    main()
