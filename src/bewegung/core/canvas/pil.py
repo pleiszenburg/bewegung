f28e25c3a930bf3bb7a0aa2d36e1fd2e3a0f0dcb
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-08-19 22:21:44 +0200

    pil canvas

---
+++ src/bewegung/core/canvas/pil.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+BEWEGUNG
+a versatile video renderer
+https://github.com/pleiszenburg/bewegung
+
+    src/bewegung/core/canvas/pil.py: PIL canvas
+
+    Copyright (C) 2020 Sebastian M. Ernst <ernst@pleiszenburg.de>
+
+<LICENSE_BLOCK>
+The contents of this file are subject to the GNU Lesser General Public License
+Version 2.1 ("LGPL" or "License"). You may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+https://www.gnu.org/licenses/old-licenses/lgpl-2.1.txt
+https://github.com/pleiszenburg/bewegung/blob/master/LICENSE
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for the
+specific language governing rights and limitations under the License.
+</LICENSE_BLOCK>
+
+"""
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# IMPORT
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+from typing import Any, Callable
+
+from PIL.Image import Image, new
+from typeguard import typechecked
+
+from ._base import CanvasBase
+from ..abc import VideoABC
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# CLASS
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+@typechecked
+class Canvas(CanvasBase):
+
+    def prototype(self, video: VideoABC, **kwargs) -> Callable:
+
+        if 'mode' not in kwargs.keys():
+            kwargs['mode'] = 'RGBA'
+        if 'size' not in kwargs.keys():
+            kwargs['size'] = (video.width, video.height)
+
+        return lambda: new(**kwargs)
+
+    def isinstance(self, obj: Any) -> bool:
+
+        return isinstance(obj, Image)
+
+    def to_pil(self, obj: Image) -> Image:
+
+        assert obj.mode == 'RGBA'
+
+        return obj
