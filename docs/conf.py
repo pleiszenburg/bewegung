37923daf482f3333870762fa882cf32c2817f154
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-11-13 18:43:52 +0100

    activate autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -61,6 +61,9 @@ release = get_version()
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_autodoc_typehints',
     'sphinx_rtd_theme',
 ]
 
@@ -84,3 +87,5 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+always_document_param_types = True # sphinx_autodoc_typehints
