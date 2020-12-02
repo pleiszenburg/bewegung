4ac218afb5edf8fc67c628e3b08e83c8fd6f2d57
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-12-02 20:02:31 +0100

    new release cycle

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 # Changes
 
+## 0.0.2 (2020-12-XX)
+
+- (TBD)
+
 ## 0.0.1 (2020-12-02)
 
 - Initial release.
--- src/bewegung/__init__.py
+++ src/bewegung/__init__.py
@@ -28,7 +28,7 @@ specific language governing rights and limitations under the License.
 # EXPORT
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 
 from .core.camera import Camera
 from .core.backends import *
