51cfc158fe976da5e7b50f9933b12e266c2c3f46
Sebastian M. Ernst <ernst@pleiszenburg.de>
2020-12-14 15:40:07 +0100

    doc strings

--- src/bewegung/core/vector/single2ddist.py
+++ src/bewegung/core/vector/single2ddist.py
@@ -42,55 +42,96 @@ from ..abc import PyNumber, Vector2DABC
 @typechecked
 class Vector2Ddist(Vector2D):
     """
-    Immutable version of Vector2D with distance parameter
+    Immutable version of :class:`bewegung.Vector2D` with distance parameter
+
+    Args:
+        x : x component. Must have the same type like ``y`` and ``dist``.
+        y : y component. Must have the same type like ``x`` and ``dist``.
+        dist : Distance component. Must have the same type like ``x`` and ``y``.
+        dtype : Data type. Derived from ``x`` and ``y`` if not explicitly provided.
     """
 
     def __init__(self, x: PyNumber, y: PyNumber, dist: PyNumber, dtype: Union[Type, None] = None):
+
         super().__init__(x = x, y = y, dtype = dtype)
         assert isinstance(dist, self._dtype)
         self._dist = dist
 
     def __repr__(self) -> str:
+        """
+        String representation for interactive use
+        """
+
         if self._dtype == int:
             return f'<Vector2Ddist x={self._x:d} y={self._y:d} dist={self._dist:d} dtype={self._dtype.__name__:s}>'
         return f'<Vector2Ddist x={self._x:e} y={self._y:e} dist={self._dist:e} dtype={self._dtype.__name__:s}>'
 
     def mul(self, scalar: PyNumber):
+        ""
         raise NotImplementedError()
 
     def as_vector(self) -> Vector2DABC:
+        """
+        Exports a vector without distance component
+        """
+
         return Vector2D(self._x, self._y, dtype = self._dtype)
 
     def copy(self) -> Vector2DABC:
+        """
+        Copies vector with distance component
+        """
+
         return type(self)(self._x, self._y, self._dist, dtype = self._dtype)
 
     def update(self, x: PyNumber, y: PyNumber):
+        ""
         raise NotImplementedError()
 
     def update_from_vector(self, other: Vector2DABC):
+        ""
         raise NotImplementedError()
 
     @property
     def x(self) -> PyNumber:
+        """
+        x component
+        """
+
         return self._x
+
     @x.setter
     def x(self, value: PyNumber):
+        ""
         raise NotImplementedError()
 
     @property
     def y(self) -> PyNumber:
+        """
+        y component
+        """
+
         return self._y
+
     @y.setter
     def y(self, value: PyNumber):
+        ""
         raise NotImplementedError()
 
     @property
     def dist(self) -> PyNumber:
+        """
+        Distance component
+        """
+
         return self._dist
+
     @dist.setter
     def dist(self, value: PyNumber):
+        ""
         raise NotImplementedError()
 
     @classmethod
     def from_polar(cls, radius: PyNumber, angle: PyNumber) -> Vector2DABC:
+        ""
         raise NotImplementedError()
