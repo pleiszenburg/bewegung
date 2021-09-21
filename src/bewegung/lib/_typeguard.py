0a4a2cb02202c52a38bd56673686904dec3796d9
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-09-21 16:48:46 +0200

    isolated typeguard into lib (2)

--- src/bewegung/lib/_typeguard.py
+++ src/bewegung/lib/_typeguard.py
@@ -6,7 +6,7 @@ BEWEGUNG
 a versatile video renderer
 https://github.com/pleiszenburg/bewegung
 
-    src/bewegung/core/typeguard.py: Wrapper around typeguard (optional)
+    src/bewegung/lib/_typeguard.py: Wrapper around typeguard (optional)
 
     Copyright (C) 2020-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
