2b851110e7e0df70b0202a8c1f98a8c2c1d55e12
Sebastian M. Ernst <ernst@pleiszenburg.de>
2021-09-21 16:38:05 +0200

    fix typo

--- src/bewegung/linalg/__init__.py
+++ src/bewegung/linalg/__init__.py
@@ -6,7 +6,7 @@ BEWEGUNG
 a versatile video renderer
 https://github.com/pleiszenburg/bewegung
 
-    src/bewegung/linalg/__init__.py: Linar algebra module root
+    src/bewegung/linalg/__init__.py: Linear algebra module root
 
     Copyright (C) 2020-2021 Sebastian M. Ernst <ernst@pleiszenburg.de>
 
